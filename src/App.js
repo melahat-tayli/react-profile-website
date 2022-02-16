32317d42db0225e8ab58d20eee236bbe73552d76
melahat-tayli <sahinmelahat@hotmail.com>
2022-02-15 20:05:05 -0800

    feat: added routing property for single page-multiple views. Name of the research-main and software-main changed and their content was updated.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import { Layout } from "./components/layout";
 import { Header } from "./components/header";
 import { Main } from "./components/main";
+import { Research } from "./components/research";
+import { Software } from "./components/software";
 import { Footer } from "./components/footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
   return (
-    <Layout>
-      <Header />
-      <Main />
-      <Footer />
-    </Layout>
+    <Router>
+      <Layout>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/software" element={<Software />} />
+          <Route path="/research" element={<Research />} />
+        </Routes>
+        <Footer />
+      </Layout> 
+    </Router>
   );
 };
 
---
+++ src/components/software.js
@@ -0,0 +1,25 @@
+import { Container } from "./container";
+export const Software = () => {
+  return (
+    <div className="softwareContent">
+      <Container>
+        <div>
+          <h2>Software Skills</h2>
+          <ul>
+            <li>Python</li>
+            <li>SQL</li>
+            <li>git</li>
+            <li>CI/CD</li>
+            <li>Pandas</li>
+            <li>scikit-learn</li>
+            <li>Luigi</li>
+            <li>NumPy</li>
+            <li>matplotlib</li>
+            <li>Unix</li>
+            <li>Cookiecutter</li>
+          </ul>
+        </div>
+      </Container>
+    </div>
+  );
+};
